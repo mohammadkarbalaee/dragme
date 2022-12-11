c3f6720fabae5a53bc63f4a4ffa4c599af2d140c
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-12-11 21:30:25 +0330

    loading bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,19 @@ void main() {
  * TODO: make an square in the middle of the screen ✅
  * TODO: make the square draggable ✅
  * TODO: make the square bouncing ✅
- * TODO: make the square contain image
- * TODO: get the image from and api from the internet
- * TODO: make the image changeable on tap
+ * TODO: make the square contain image ✅
+ * TODO: get the image from and api from the internet ✅
+ * TODO: make the image changeable on tap ✅
  * TODO: make the square bounce on wall collision ✅
  * TODO: add sound effect on the collision ✅
  * TODO: add vibration on tap ✅
- * TODO: store the state and position of the object inside shared preferences
- * TODO: add an appbar and a menu to showcase your skills more
- * TODO: add notifications to the app
- * TODO: write test for the app
- * TODO: do each part of the code in one branch and then merge it
- * TODO: make the UI better looking
+ * TODO: do each part of the code in one branch and then merge it ✅
+ * TODO: make the UI better looking ✅
  * TODO: add splash screen
  * TODO: add a logo to the app
  * TODO: make the code cleaner
  * TODO: document the project
+ * TODO: Deploy it on web too and send them the URL also
  * TODO: make a readme for the project and describe it very well
  * TODO: delete this todo list from the source code
  * TODO: send the project
